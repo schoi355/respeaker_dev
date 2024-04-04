938bb5393d172a7cfbba907e9a3a37c48412c467
Preet Sanghavi <54236288+Preet538-neitzen@users.noreply.github.com>
2024-04-04 13:53:11 -0500

    Update record_DOA_ID_chunks.py

--- record_DOA_ID_chunks.py
+++ record_DOA_ID_chunks.py
@@ -9,6 +9,9 @@ import time
 from datetime import datetime
 import boto3
 import os
+import subprocess
+from decimal import Decimal
+from collections import defaultdict
 
 RESPEAKER_RATE = 16000
 RESPEAKER_CHANNELS = 1 # change base on firmwares, 1_channel_firmware.bin as 1 or 6_channels_firmware.bin as 6
@@ -43,21 +46,21 @@ def ang_shift(angle):
     return shifted_angle
 
 # Assign a range of angles for each speaker 
-def assign_angle(number, ID_file):
+def assign_angle(ID_file):
     angle = int(20) # 20 deg 
     ang_dic = {}
     with open(ID_file, 'r') as f:
         ID_data = json.load(f)
-        if len(ID_data) == number:
-            for key in ID_data:
-                angle_range = [ID_data[key]['doa']-angle, ID_data[key]['doa']+angle]
-                ang_dic[ID_data[key]['ID'][0]] = angle_range
+        
+        for key in ID_data:
+            angle_range = [ID_data[key]['doa']-angle, ID_data[key]['doa']+angle]
+            ang_dic[ID_data[key]['ID'][0]] = angle_range
 
-            print('The range of angles are assigned:')
-            return ang_dic
+        print('The range of angles are assigned:')
+        return ang_dic
 
-        else:
-            print('The number of people does not match with the number of IDs')
+        # else:
+        #     print('The number of people does not match with the number of IDs')
 
 def find_device():
     dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
@@ -76,11 +79,10 @@ def open_audio_stream(p):
     return stream
 
 
-def record_audio(stream, p, dev, num, ID_file, audio_file, doa_file):
+def record_audio(stream, p, dev, num, ID_file, audio_file, doa_file, unknown_speakers):
     data_list = []
     count = 0
-
-    ang_dic = assign_angle(num, ID_file)
+    ang_dic = assign_angle(ID_file)
     print(ang_dic)
 
     wf = wave.open(audio_file, 'wb')
@@ -99,10 +101,25 @@ def record_audio(stream, p, dev, num, ID_file, audio_file, doa_file):
                 timestamp = time.time()
 
                 # Assign a speaker according to DOA
-                ID = 'unknown'
+                ID = None
                 for key in ang_dic:
                     if ang_shift(ang_dic[key][0]) <= ang_shift(doa) <= ang_shift(ang_dic[key][1]):
                         ID = key
+                
+                # If no known speaker matches, check unknown speakers
+                if ID is None:
+                     for us_id, details in unknown_speakers.items():
+                        #print(doa, ang_shift(details['start']), ang_shift(details['end']))
+                        if ang_shift(details['start']) <= ang_shift(doa) <= ang_shift(details['end']):  # Adjusting for ±5 degrees
+                            ID = us_id
+                            break
+
+                # If no match found, assign a new temporary ID
+                if ID is None:
+                    ID = f'temp_{len(unknown_speakers) + 1}'
+                    unknown_speakers[ID]['start'] = min(unknown_speakers[ID]['start'], doa - 20)
+                    unknown_speakers[ID]['end'] = max(unknown_speakers[ID]['end'], doa + 20)
+                    unknown_speakers[ID]['ids'].append(ID)
 
                 data_list.append({'doa': doa, 'timestamp': timestamp, 'record_time': count/10, 'speaker': ID})
                 print(str(count/10) + ', ' + str(doa), " ", ID)
@@ -110,8 +127,11 @@ def record_audio(stream, p, dev, num, ID_file, audio_file, doa_file):
              
             with open (doa_file, 'w') as fj:
                 json.dump(data_list,fj)
-    
-    print("chunk saved")
+
+    return unknown_speakers
+
+
+
 
 def close_audio_stream(stream, p):
     stream.stop_stream()
@@ -133,12 +153,79 @@ def word_to_num(word):
     }
     return mapping.get(word.lower(), 0)
 
+
+def upload_json_to_dynamodb(id_file, table_name):
+    # Initialize a DynamoDB resource
+    dynamodb = boto3.resource('dynamodb', region_name=AWS_REGION,
+                              aws_access_key_id=AWS_ACCESS_KEY_ID,
+                              aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+    table = dynamodb.Table(table_name)
+
+    # Load ID.json file
+    with open(id_file, 'r') as file:
+        id_data = json.load(file)
+
+    # Aggregate speaker information into a list or map
+    speakers_list = []
+    for speaker_id, info in id_data.items():
+        
+        # Each entry in the list is a map/dictionary of speaker information
+        speaker_info = {
+            'SpeakerID': speaker_id,
+            'DOA': Decimal(str(info['doa'])),
+            'ID': info['ID']
+        }
+        speakers_list.append(speaker_info)
+
+    # Prepare the item for DynamoDB
+    # Assuming 'SessionID' is the partition key, and 'Speakers' will store the list of speaker information
+    item = {
+        'SessionID': '3',
+        'Speakers': speakers_list
+    }
+
+    # Upload the item to DynamoDB
+    response = table.put_item(Item=item)
+    print(f"Uploaded session 3 with all speakers to DynamoDB")
+
+def update_id_json(id_file, unknown_speakers):
+
+    print(unknown_speakers)
+    try:
+        with open('assign_speaker/' + id_file, 'r') as file:
+            id_data = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        print(f"Error opening or reading {id_file}: {e}")
+        id_data = {}
+
+    for speaker_id, details in unknown_speakers.items():
+        doa_avg = Decimal((details['start'] + details['end']) / 2)
+        id_data[speaker_id] = {
+            "doa": float(doa_avg),
+            "ID": [str(speaker_id), "Random animal", str(speaker_id.split('_')[1])]
+        }
+
+    try:
+        with open('assign_speaker/ID.json', 'w') as file:
+            json.dump(id_data, file, indent=4)
+        print(f"{id_file} updated with unknown speakers.")
+    except Exception as e:
+        print(f"Failed to update {id_file}: {e}")
+
 def main():
+
+    # subprocess.run(["python", "assign_speaker.py"], check=True)
+    
+    # # After assign_speaker.py completes, proceed with the rest of this script
+    # print("assign_speaker.py has finished. Proceeding with the next part.")
+
+
     ID_file  = 'assign_speaker/ID.json'
     num = int(get_ID_number())
     sec = int(get_sec())
     os.environ['LAST_ITERATION'] = str(sec)
     iteration = 0
+    unknown_speakers = defaultdict(lambda: {'start': float('inf'), 'end': float('-inf'), 'ids': []})
 
     # Load IDs from the ID file
     with open(ID_file, 'r') as f:
@@ -147,11 +234,13 @@ def main():
         numeric_ids = sorted([word_to_num(info['ID'][2]) for info in ID_data.values()])
         id_str = '_'.join(map(str, numeric_ids))
 
-
+    
     # Start recording
     while True:
         if iteration >= sec:
             print("DONE RECORDING")
+            
+            upload_json_to_dynamodb('assign_speaker/ID.json', 'Team_assignment')
             break
         else:
             dev = find_device()
@@ -163,17 +252,17 @@ def main():
 
             print("RECORDING STARTED")
                 
-            record_audio(stream, p, dev, num, ID_file, audio_file, doa_file)
-
+            unknown_speakers = (record_audio(stream, p, dev, num, ID_file, audio_file, doa_file, unknown_speakers))
+            update_id_json('ID.json', unknown_speakers)
             date_folder = datetime.now().strftime('%Y-%m-%d')
             audio_s3_path = f'audio-files/{date_folder}/{id_str}/{os.path.basename(audio_file)}'
             doa_s3_path = f'doa-files/{date_folder}/{id_str}/{os.path.basename(doa_file)}'
             
-            # # Upload audio file to S3
-            # upload_to_s3(audio_file, audio_s3_path)
+            # Upload audio file to S3
+            upload_to_s3(audio_file, audio_s3_path)
 
             # # Upload doa file to S3
-            # upload_to_s3(doa_file, doa_s3_path)
+            upload_to_s3(doa_file, doa_s3_path)
 
             close_audio_stream(stream, p)
 
