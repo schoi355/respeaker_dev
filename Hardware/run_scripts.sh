b342a470c101ee3e69518c15f2d85c1c58c311d5
schoi355 <csyzzang52@gmail.com>
2024-10-16 11:09:45 -0500

    Move files to Hardware

---
+++ Hardware/run_scripts.sh
@@ -0,0 +1,3 @@
+gnome-terminal --tab -- bash -ic "source transcribe.sh; exec bash"
+gnome-terminal --tab -- bash -ic "source flask.sh; exec bash"
+gnome-terminal --tab -- bash -ic "source record.sh; exec bash"
